e712fd3a63669de4c64f8e58cab803821b809c1b
phillipesimon <phillipe.simon@gmail.com>
2022-11-06 14:47:15 -0300

    Aula 01

---
+++ src/screens/SingIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from 'native-base';
+
+export function SingIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24} fontFamily="heading">
+        SingIn-Teste
+      </Text>
+    </Center>
+  );
+}
